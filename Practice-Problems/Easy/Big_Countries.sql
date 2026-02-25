6b208cd565fb987d3da69b64ef28502d15e33570
uday-vashist <udayvashist4@gmail.com>
2026-02-25 23:50:32 +0530

    Fix SQL query formatting for Big Countries

--- Practice-Problems/Easy/Big_Countries.sql
+++ Practice-Problems/Easy/Big_Countries.sql
@@ -9,7 +9,7 @@
 -- population >= 25,000,000 OR area >= 3,000,000.
 -- We return name, population, and area for such countries.
 
-SELECT name , population , area 
+SELECT name , population , area
   FROM World
   WHERE Area >= 3000000
   OR Population >= 25000000 ;
